c9ba7032ddc2966a954d0a84a36213257774619e
Kosiposha12 <fedya.makritsa@gmail.com>
2022-04-28 10:15:06 +0300

    First commit

---
+++ lab6n7/lab6n7/Classes/ProductRepositoryViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
+
+namespace lab6n7.Classes
+{
+    public class ProductRepositoryViewModel
+    {
+        private ICommand _generateProducts;
+        private ICommand _clearProducts;
+        private ICommand _saveProducts;
+        private ICommand _loadProducts;
+        private ICommand _showAll;
+        private ICommand _showFiltered;
+
+        public AdvertInGrid ProductFilter { get; set; }
+        public ObservableCollection<Advert> Adverts{ get; set; }
+        public ObservableCollection<Advert> FilteredProducts { get; set; }
+
+        //public ICommand ShowAll
+        //{
+        //    get
+        //    {
+        //        if (_showAll == null)
+        //        {
+        //            _showAll = new RelayCommand(x =>
+        //           {
+        //               FilteredProducts.Clear();
+        //               foreach (var Advert in Adverts)
+        //               {
+        //                   FilteredProducts.Add(Advert);
+        //               }
+        //           });
+
+
+
+
+        //        }
+
+        //        return _showAll;
+        //    }
+        //}
+
+
+    }
+}
---
+++ lab6n7/lab6n7/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+
+namespace lab6n7
+{
+   
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        static Cursor C1 = new Cursor(Application.GetResourceStream(new Uri("Images/arrow.cur", UriKind.Relative)).Stream);
+
+        public string languagePath;
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+           R1.Cursor = C1;
+
+           
+        }
+
+      
+
+       
+
+        private void ButtonPopUpLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+       
+
+        private void ButtonFAQ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Version 1.0 ©KriSWhitch", "FAQ", MessageBoxButton.OK);
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Children.Clear();
+            GridMain.Children.Add(new HomePage(this));
+        }
+
+        private void CreateAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Children.Clear();
+            GridMain.Children.Add(new CreateAdPage());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HomeButton_Click(sender, e);
+        }
+
+       
+
+        private void RU_Click(object sender, RoutedEventArgs e)
+        {
+            languagePath = "Dictionaries/Ru.Xaml";
+            ResourceDictionary language = new ResourceDictionary();
+            language.Source = new Uri(languagePath, UriKind.Relative);
+            Resources.MergedDictionaries.Add(language);
+        }
+
+        private void EN_Click(object sender, RoutedEventArgs e)
+        {
+            languagePath = "Dictionaries/En.Xaml";
+            ResourceDictionary language = new ResourceDictionary();
+            language.Source = new Uri(languagePath, UriKind.Relative);
+            Resources.MergedDictionaries.Add(language);
+        }
+    }
+}
